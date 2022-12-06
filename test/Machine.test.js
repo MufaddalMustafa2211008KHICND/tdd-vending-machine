67154f451c75246f7c905d1552d48fb49feb05f5
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2022-12-06 16:54:58 +0500

    AC2 written failed test

--- test/Machine.test.js
+++ test/Machine.test.js
@@ -14,15 +14,25 @@ describe('the vending machine', () => {
     //     expect(true).toEqual(true);
     // });
 
-    test('At first I should see selections in the vending machine', () => {
+    test('AC1: At first I should see selections in the vending machine', () => {
         //arrange
         const vm =  new Machine;
 
         //act
         const selections = vm.seeSelections();
-        
+
         //assert
         expect(selections.length).not.toBe(0)
     })
 
+    test('AC2: When I deposit money, the machine shows the deposited amount', () => {
+        //arrange
+        const vm =  new Machine;
+
+        //act
+        const depositedMoney = vm.deposit(500);
+
+        //assert
+        expect(depositedMoney).not.toBe('You have deposited Rs 500')
+    })
 });
