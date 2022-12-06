b8d625856151685fa88bbebf8f1eb8494281ee55
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2022-12-06 16:44:00 +0500

    first test passed for AC1

--- src/Machine.js
+++ src/Machine.js
@@ -5,5 +5,10 @@ module.exports = class Machine {
 
 
     seeSelections() {
+        return [
+            {item:'crisps', price:'Rs 100'}, 
+            {item: 'chocolate', price:'Rs 350'},
+            {item: 'mints', price: 'Rs 70'}
+        ]
     }
 };
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,16 +1,28 @@
+const { TestScheduler } = require('jest');
 const Machine = require('../src/Machine');
 
 describe('the vending machine', () => {
-    it('should have items to purchase', () => {
-        // setup
-        const machine = new Machine();
-        const expected = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
+    // it('should have items to purchase', () => {
+    //     // setup
+    //     const machine = new Machine();
+    //     const expected = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
 
-        // exercise
-        const actual = machine.seeSelections();
+    //     // exercise
+    //     const actual = machine.seeSelections();
 
-        // assert
-        expect(true).toEqual(true);
-    });
+    //     // assert
+    //     expect(true).toEqual(true);
+    // });
+
+    test('At first I should see selections in the vending machine', () => {
+        //arrange
+        const vm =  new Machine;
+
+        //act
+        const selections = vm.seeSelections();
+
+        //assert
+        expect(selections.length).not.toBe(0)
+    })
 
 });
